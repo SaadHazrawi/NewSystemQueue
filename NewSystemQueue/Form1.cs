8037be55520b6d4b508420ee12be6e04e7bbc2a4
saadhzw <saadhzw@gmail.com>
2024-11-16 23:54:14 +0300

    Add project files.

---
+++ NewSystemQueue/Form1.cs
@@ -0,0 +1,20 @@
+﻿using NewSystemQueue.View;
+using System;
+using System.Windows.Forms;
+
+namespace NewSystemQueue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMM1_Click(object sender, EventArgs e)
+        {
+            MM1Queue mM1Queue = new MM1Queue();
+              mM1Queue.ShowDialog();
+        }
+    }
+}
